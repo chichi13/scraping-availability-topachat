986c2e7fe49a0b75504d9268a61b9141f381442a
Antoine Chiris <antoinechiris@gmail.com>
2022-04-08 19:34:08 +0200

    Renamed function

--- resources/core.py
+++ resources/core.py
@@ -11,36 +11,38 @@ from resources.config import settings
 async def send_ifttt_notification(name, price, url, storage):
     report = {}
 
-    if url not in storage:
-        logging.info("Send notification for %s", name)
+    if url in storage:
+        return
 
-        report["value1"] = name
-        report["value2"] = price
-        report["value3"] = url
-        async with aiohttp.ClientSession() as session:
-            await session.post(settings.IFTTT_WEBHOOK_URL, data=report)
-        storage.set_key(url, timedelta(hours=settings.TIME_TO_EXPIRE))
+    logging.info("Send notification for %s", name)
+
+    report["value1"] = name
+    report["value2"] = price
+    report["value3"] = url
+    async with aiohttp.ClientSession() as session:
+        await session.post(settings.IFTTT_WEBHOOK_URL, data=report)
+    storage.set_key(url, timedelta(hours=settings.TIME_TO_EXPIRE))
 
 
-async def inspect_content(data, url, storage):
+async def inspect_and_send(data, url, storage):
     soup = BeautifulSoup(data, features="lxml")
 
     name = soup.find("h1", attrs={"class": "fn"})
     price = soup.find("span", attrs={"class": "priceFinal fp44"})
-    logging.info(f"Scraping product : {name.text}...")
     availability = soup.find("section", attrs={"class": "cart-box en-rupture"})
+    logging.info(f"Scraping product : {name.text}...")
 
     if availability is not None:
         logging.info(f"{name.text} en rupture de stock.")
-    else:
-        await send_ifttt_notification(name.text, price.text, url, storage)
+        return
+    await send_ifttt_notification(name.text, price.text, url, storage)
 
 
 async def fetch_all(url, storage):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             data = await resp.text()
-            await inspect_content(data, url, storage)
+            await inspect_and_send(data, url, storage)
 
 
 async def get_url(storage):
